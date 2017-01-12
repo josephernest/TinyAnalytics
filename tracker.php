1fdb491c04cec8a38dd50dfdab3e3edd29d95cf4
josephernest <nouvellecollection@gmail.com>
2017-01-12 11:04:37 +0100

    Now tracker.php stores .log's datetime in UTC timestamp
    Related to https://github.com/josephernest/TinyAnalytics/issues/9

--- tracker.php
+++ tracker.php
@@ -2,7 +2,7 @@
 function record_visit($sitename)
 {
     $logfile = realpath(dirname(__FILE__)) . '/logs/' . $sitename . '.log';
-    $txt = date("Y-m-d") . "\t" . date("H:i:s") . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . $_SERVER['HTTP_REFERER'] . PHP_EOL;
+    $txt = time() . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . $_SERVER['HTTP_REFERER'] . PHP_EOL;
     file_put_contents($logfile, $txt, FILE_APPEND);
 }
 ?>

9e6703511a460098b73a25c98c6c43c9168e8970
josephernest <nouvellecollection@gmail.com>
2017-05-21 20:49:57 +0200

    Update tracker.php

--- tracker.php
+++ tracker.php
@@ -14,7 +14,7 @@ function summarize($force = false)
 function record_visit($sitename)
 {
     $logfile = realpath(dirname(__FILE__)) . '/logs/' . $sitename . '.log';
-    $txt = time() . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '') . PHP_EOL;
+    $txt = time() . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '') . "\t" . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '') . PHP_EOL;
     file_put_contents($logfile, $txt, FILE_APPEND);
     summarize();
 }
